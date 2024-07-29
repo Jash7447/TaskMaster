0df82d75733badf06ea7fc225ee19cf1176cb05c
Jash7447 <jashparikh7447@gmail.com>
2024-07-29 19:26:59 +0530

    sidebar for web and burger for mobile, signout

--- app/loggedin/page.tsx
+++ app/loggedin/page.tsx
@@ -1,14 +1,20 @@
+import MobileNav from "@/components/nav/mobile-nav";
+import { SideBar } from "@/components/nav/side_bar";
 import Tasks from "@/components/taskmaster/tasks";
-import UserProfile from "@/components/taskmaster/user-profile";
+import UserProfile from "@/components/nav/user-profile";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-between p-24">
-      <h1>Taskmaster</h1>
-      <UserProfile/>
-      <Tasks />
-    </main>
+    <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
+      <SideBar/>
+      <div className="flex flex-col">
+        <MobileNav/>
+        <main className="flex flex-1 flex-col gap-4 p-4 lg:px-8">
+          <h1>Taskmaster</h1>
+        </main>
+      </div>
+    </div>
   );
 }
