bb80136d8d59e6eeefc660e521b95716b15be8d2
Jash7447 <jashparikh7447@gmail.com>
2024-07-31 21:13:59 +0530

    backend for intergrating openAI

--- app/loggedin/today/page.tsx
+++ app/loggedin/today/page.tsx
@@ -1,20 +1,16 @@
+import Today from "@/components/containers/today";
 import MobileNav from "@/components/nav/mobile-nav";
 import { SideBar } from "@/components/nav/side-bar";
-import Tasks from "@/components/taskmaster/tasks";
-import UserProfile from "@/components/nav/user-profile";
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
 import TodoList from "@/components/todos/todo-list";
 
 export default function Home() {
   return (
     <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
-      <SideBar/>
+      <SideBar />
       <div className="flex flex-col">
-        <MobileNav/>
+        <MobileNav />
         <main className="flex flex-1 flex-col gap-4 p-4 lg:px-8">
-          <h1>Taskmaster</h1>
-          <TodoList/>
+          <Today />
         </main>
       </div>
     </div>
